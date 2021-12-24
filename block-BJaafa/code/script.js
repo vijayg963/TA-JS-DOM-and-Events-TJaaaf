a4424fc60a70f21715e25d4a6d0ce7150b055489
vijayg963 <vijayg963@gmail.com>
2021-12-24 10:24:20 +0530

    Final-project

---
+++ block-BJaafa/code/script.js
@@ -0,0 +1,21 @@
+let secondBox = document.querySelector(".second");
+let firstBoxes = document.querySelectorAll(".first li");
+
+firstBoxes.forEach((box, index) => {
+  box.addEventListener("click", (event) => {
+    event.target.innerText = index + 1;
+
+    setTimeout(() => {
+      event.target.innerText = "";
+    }, 5000);
+  });
+});
+
+secondBox.addEventListener("click", (event) => {
+  let text = event.target.dataset.text;
+  event.target.innerText = text;
+
+  setTimeout(() => {
+    event.target.innerText = "";
+  }, 5000);
+});
